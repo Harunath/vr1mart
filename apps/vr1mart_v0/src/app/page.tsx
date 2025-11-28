6c16fb37f760df27137b21d369017b9df4f3191f
harunath <harunath04@gmail.com>
2025-11-28 12:42:06 +0530

    first commit

---
+++ apps/vr1mart_v0/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<div className=" text-7xl font-bold ">
+			<h1 className=" text-green-500">Welcome to VR1Mart</h1>
+		</div>
+	);
+}
